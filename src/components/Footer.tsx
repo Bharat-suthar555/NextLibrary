47ca40dc07da311ac6de24ea8274a7c4f3ba7d36
himanshu hada <himanshuhada17@gmail.com>
2024-01-18 18:01:29 +0530

    Changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,6 +5,7 @@ const Footer: React.FC = () => {
   return (
     <footer className='bg-gray-800 text-white text-center py-4 mt-10 fixed bottom-0 w-full z-50'>
       <p>&copy; 2023 Library Management System</p>
+      <p>Hello</p>
     </footer>
   );
 };
